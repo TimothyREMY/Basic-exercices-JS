e50a876b2edcd855458796ac03b5dfe9c21924ae
TimothyREMY <timothy.remy@gmail.com>
2021-05-27 18:36:19 +0200

    last commit

--- script_7.js
+++ script_7.js
@@ -1,5 +1,24 @@
-answer = prompt("Entrez une phrase")
+let answer = prompt("Entrez une phrase");
 
-if(answer[last] === "?"){
-  console.log("Ouais ouais...")
+let questionMark = answer.slice(-1)
+
+if(questionMark == "?"){
+  console.log("Ouais ouais...");
 }
+
+
+
+else if(answer === answer.toUpperCase() && answer !== ""){
+  console.log("Pwa, calme-toi...")
+}
+
+else if(answer.includes("Fortnite")) {
+  console.log("on s' fait une partie soum-soum ?" )
+}
+
+else if(answer === ""){
+  console.log("t'es en PLS ?")
+}
+
+else
+{console.log("balek.")}
